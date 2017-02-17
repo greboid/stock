d674ac15cf82e5fb59fe074e11514533fcf08794
Greg Holmes <git@greg.holmes.name>
2017-02-17 00:18:53 +0000

    Move the config settings about and add some comments.

--- config.php
+++ config.php
@@ -4,6 +4,32 @@ if (file_exists(dirname( __FILE__ ).'/config.local.php')) {
     include(dirname( __FILE__ ).'/config.local.php');
 }
 
+#You probably want to define this in config.local.php
+if (!defined('STOCK_DB')) {
+    define('STOCK_DB', 'stock');
+}
+if (!defined('STOCK_DB_USER')) {
+    define('STOCK_DB_USER', 'stock');
+}
+if (!defined('STOCK_DB_PW')) {
+    define('STOCK_DB_PW', 'stock');
+}
+if (!defined('STOCK_DB_HOST')) {
+    define('STOCK_DB_HOST', '127.0.0.1');
+}
+
+#You might want to change these but probably not
+##Database settings
+if (!defined('SITES_TABLE')) {
+    define('SITES_TABLE', 'sites');
+}
+if (!defined('LOCATIONS_TABLE')) {
+    define('LOCATIONS_TABLE', 'locations');
+}
+if (!defined('STOCK_TABLE')) {
+    define('STOCK_TABLE', 'stock');
+}
+##Paths
 if (!defined('LIBS_PATH')) {
     define('LIBS_PATH', '../libs/');
 }
@@ -18,25 +44,4 @@ if (!defined('CACHE_PATH')) {
 }
 if (!defined('CONFIG_PATH')) {
     define('CONFIG_PATH', '../configs/');
-}
-if (!defined('SITES_TABLE')) {
-    define('SITES_TABLE', 'sites');
-}
-if (!defined('LOCATIONS_TABLE')) {
-    define('LOCATIONS_TABLE', 'locations');
-}
-if (!defined('STOCK_TABLE')) {
-    define('STOCK_TABLE', 'stock');
-}
-if (!defined('STOCK_DB')) {
-    define('STOCK_DB', 'stock');
-}
-if (!defined('STOCK_DB_USER')) {
-    define('STOCK_DB_USER', 'stock');
-}
-if (!defined('STOCK_DB_PW')) {
-    define('STOCK_DB_PW', 'stock');
-}
-if (!defined('STOCK_DB_HOST')) {
-    define('STOCK_DB_HOST', '127.0.0.1');
 }
