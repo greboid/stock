6960d991f50b9a09cef18c47befd990bf6fce2d7
Greg Holmes <git@greg.holmes.name>
2017-03-05 21:25:11 +0000

    Rewrite the getCategories function to get all sub categories.
    This could be recursive, but that annoyed me, this seems to work.

--- sss/stock.php
+++ sss/stock.php
@@ -235,48 +235,25 @@
         }
 
         public function getCategories(): array {
-            $selectClause = 'SELECT categories.category_id AS category_id, parents.category_name AS category_parent, COALESCE(categories.category_name, "") AS category_name
-                                                      FROM '.CATEGORIES_TABLE.' as categories
-                                                      LEFT JOIN '.CATEGORIES_TABLE.' as parents ON categories.category_parent=parents.category_id';
-            $sql = $selectClause.' WHERE categories.category_parent = 0 OR categories.category_parent IS NULL
-                                                      ORDER BY category_name';
+            $selectClause = 'SELECT categories.category_id AS category_id, COALESCE(parents.category_id, 0) AS category_parent, COALESCE(categories.category_name, "") AS category_name
+                                                              FROM '.CATEGORIES_TABLE.' as categories
+                                                              LEFT JOIN '.CATEGORIES_TABLE.' as parents ON categories.category_parent=parents.category_id';
+            $sql = $selectClause.' ORDER BY category_parent, category_name';
             $statement = $this->dbconnection->prepare($sql);
             $statement->execute();
             $statement->bind_result($categoryID, $categoryParent, $categoryName);
-
+            $statement->store_result();
+            $refs = array();
             $categories = array();
             while ($statement->fetch()) {
-                $categories[$categoryID] = array('id'=>$categoryID, 'name'=>$categoryName, 'parent'=>$categoryParent, 'subcategories'=>array());
-            }
-            $statement->close();
-            foreach ($categories as $key => &$category) {
-                $sql = $selectClause.' WHERE categories.category_parent = ?';
-                $statement = $this->dbconnection->prepare($sql);
-                $statement->bind_param('i', $key);
-                $statement->execute();
-                $statement->bind_result($categoryID, $categoryParent, $categoryName);
-                while ($statement->fetch()) {
-                    $category['subcategories'][] = array('id'=>$categoryID, 'name'=>$categoryName, 'parent'=>$categoryParent, 'subcategories'=>array());
+                $thisref = &$refs[ $categoryID ];
+                $thisref['parent_id'] = $categoryParent;
+                $thisref['name'] = $categoryName;
+                if ($categoryParent == 0) {
+                    $categories[ $categoryID ] = &$thisref;
+                } else {
+                    $refs[ $categoryParent ]['subcategories'][ $categoryID ] = &$thisref;
                 }
-                $statement->close();
-            }
-            return $categories;
-        }
-
-        public function getSubCategories(int $parentCategory): array {
-            $sql = 'SELECT categories.category_id AS category_id, parents.category_name AS category_parent, COALESCE(categories.category_name, "") AS category_name
-                                                      FROM '.CATEGORIES_TABLE.' as categories
-                                                      LEFT JOIN '.CATEGORIES_TABLE.' as parents ON categories.category_parent=parents.category_id
-                                                      WHERE categories.category_parent=?
-                                                      ORDER BY category_name';
-            $statement = $this->dbconnection->prepare($sql);
-            $statement->bind_param('i', $parentCategory);
-            $statement->execute();
-            $statement->bind_result($categoryID, $categoryParent, $categoryName);
-
-            $categories = array();
-            while ($statement->fetch()) {
-                $categories[$categoryID] = array('name'=>$categoryName, 'parent'=>$categoryParent);
             }
             return $categories;
         }
