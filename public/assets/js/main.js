2d94a381c85d5b4b9f814a3c0f2fbb85f0445141
Greg Holmes <git@greg.holmes.name>
2017-03-10 20:28:25 +0000

    Implement sorting of stock tables.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,17 +15,21 @@ $.tablesorter.addParser({
     is: function(s) {
         return false;
     },
-    format: function(s) {
+    format: function(s, table, cell, cellIndex) {
         var regex = new RegExp('(.*?)value=\"(.*?)\"(.*?)');
         var results = regex.exec(s.toLowerCase());
-        return 0;
+        return cell.getElementsByTagName('input')[0].value;
     },
-    type: 'text'
+    type: 'numeric'
 });
 $("#stock").tablesorter({
+    textExtraction: "complex",
     headers: {
         3: {
             sorter:'inputcount'
+        },
+        4: {
+            sorter: false
         }
     }
 });
