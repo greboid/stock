b73551ffbd67fa1494182ce62df0c3441e9ee254
Greg Holmes <git@greg.holmes.name>
2017-03-25 21:19:40 +0000

    Add some semi colons.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -124,73 +124,81 @@ $("#addUserModal").on("hidden.bs.modal", function () {
 });
 
 $("#editSiteModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget)
-  var siteName = button.data("sitename")
-  var siteID = button.data("siteid")
-  var modal = $(this)
-  modal.find("#editID").val(siteID)
-  modal.find("#editName").val(siteName)
+  var button = $(event.relatedTarget);
+  var siteName = button.data("sitename");
+  var siteID = button.data("siteid");
+  var modal = $(this);
+  modal.find("#editID").val(siteID);
+  modal.find("#editName").val(siteName);
 });
 
 $("#editLocationModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget)
-  var locationid = button.data("locationid")
-  var locationname = button.data("locationname")
-  var sitename = button.data("sitename")
-  var modal = $(this)
-  modal.find("#editName").val(locationname)
-  modal.find("#editID").val(locationid)
-  $("#editSite").find("option:contains(\""+sitename+"\")").attr("selected", "selected")
+  var button = $(event.relatedTarget);
+  var locationid = button.data("locationid");
+  var locationname = button.data("locationname");
+  var sitename = button.data("sitename");
+  var modal = $(this);
+  modal.find("#editName").val(locationname);
+  modal.find("#editID").val(locationid);
+  $("#editSite").find("option:contains(\""+sitename+"\")").attr("selected", "selected");
 });
 
 $("#editCategoryModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget)
-  var categoryid = button.data("categoryid")
-  var categoryname = button.data("categoryname")
-  var categoryparent = button.data("categoryparent")
-  var modal = $(this)
-  modal.find("#editName").val(categoryname)
-  modal.find("#editID").val(categoryid)
+  var button = $(event.relatedTarget);
+  var categoryid = button.data("categoryid");
+  var categoryname = button.data("categoryname");
+  var categoryparent = button.data("categoryparent");
+  var modal = $(this);
+  modal.find("#editName").val(categoryname);
+  modal.find("#editID").val(categoryid);
   if (categoryparent == "") {
-    $("#editParent").find("option:contains(\""+categoryparent+"\")").prop("selected", false)
+    $("#editParent").find("option:contains(\""+categoryparent+"\")").prop("selected", false);
   } else {
-    $("#editParent").find("option:contains(\""+categoryparent+"\")").attr("selected", "selected")
+    $("#editParent").find("option:contains(\""+categoryparent+"\")").attr("selected", "selected");
   }
 });
 
 $("#editItemModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget)
-  var itemid = button.data("id")
-  var itemname = button.data("itemname")
-  var locationname = button.data("locationname")
-  var categoryname = button.data("categoryname")
-  var stockcount = button.data("stockcount")
-  var modal = $(this)
-  modal.find("#editID").val(itemid)
-  modal.find("#editName").val(itemname)
-  $("#editLocation option").each(function () { if ($(this).text() == locationname) { $(this).attr("selected", "selected") } });
-  $("#editCategory option").each(function () { if ($(this).text().trim() == categoryname) { $(this).attr("selected", "selected") } });
-  modal.find("#editCount").val(stockcount)
+  var button = $(event.relatedTarget);
+  var itemid = button.data("id");
+  var itemname = button.data("itemname");
+  var locationname = button.data("locationname");
+  var categoryname = button.data("categoryname");
+  var stockcount = button.data("stockcount");
+  var modal = $(this);
+  modal.find("#editID").val(itemid);
+  modal.find("#editName").val(itemname);
+  $("#editLocation option").each(function () {
+    if ($(this).text() == locationname) {
+      $(this).attr("selected", "selected");
+    }
+  });
+  $("#editCategory option").each(function () {
+    if ($(this).text().trim() == categoryname) {
+      $(this).attr("selected", "selected");
+    }
+  });
+  modal.find("#editCount").val(stockcount);
 });
 
 $("#editUserModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget)
-  var userid = button.data("userid")
-  var username = button.data("username")
-  var name = button.data("name")
-  var email = button.data("email")
-  var active = button.data("active")
-  var modal = $(this)
-  modal.find("#editID").val(userid)
-  modal.find("#editUsername").val(username)
-  modal.find("#editName").val(name)
-  modal.find("#editEmail").val(email)
+  var button = $(event.relatedTarget);
+  var userid = button.data("userid");
+  var username = button.data("username");
+  var name = button.data("name");
+  var email = button.data("email");
+  var active = button.data("active");
+  var modal = $(this);
+  modal.find("#editID").val(userid);
+  modal.find("#editUsername").val(username);
+  modal.find("#editName").val(name);
+  modal.find("#editEmail").val(email);
   if (active == 1) {
-    modal.find("#activey").attr("checked", "checked")
-    modal.find("#activen").removeAttr("checked", "checked")
+    modal.find("#activey").attr("checked", "checked");
+    modal.find("#activen").removeAttr("checked", "checked");
   } else {
-    modal.find("#activey").removeAttr("checked", "checked")
-    modal.find("#activen").attr("checked", "checked")
+    modal.find("#activey").removeAttr("checked", "checked");
+    modal.find("#activen").attr("checked", "checked");
   }
 });
 $(".itemcountbutton").click( function() {
@@ -206,13 +214,13 @@ $(".itemcountbutton").click( function() {
   }).done(function() {
     row.find("input").val(newCount);
   });
-  return false
+  return false;
 });
 $(".itemcount").change(function(){
   var row = $(this).closest("tr");
   var newCount = parseInt($(this).val());
   var itemID = row.data("itemid");
-  var data = "{\"newcount\":\""+newCount+"\"}"
+  var data = "{\"newcount\":\""+newCount+"\"}";
   $.ajax({
          "method": "POST",
          "dataType": "json",
@@ -234,21 +242,21 @@ $.fn.dataTable.ext.search.push(
       var minCount = (isNaN(min) ? true : parseInt(data[3]) >= min);
       var maxCount = (isNaN(max) ? true : parseInt(data[3]) <= max);
 
-      return itemSet && locationSet && siteSet && minCount && maxCount
+      return itemSet && locationSet && siteSet && minCount && maxCount;
     }
 );
 var table = $("#stock").DataTable({
   ordering: true,
   dom: "rtp",
 });
-$("#itemsearch").keyup( function() {
+$("#itemsearch").keyup(function() {
   table.draw();
 });
 $( "#locationsearch, #sitesearch, #mincount, #maxcount").change(function() {
   table.draw();
 });
 $("#itemsearchform, #mincountform, #maxcountform").bind("reset", function() {
-  setTimeout(function(){
+  setTimeout(function() {
         table.draw();
     }, 200);
 });
