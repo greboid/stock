d377371a3d861b3266c128c0ac5a838a92af9460
Greg Holmes <git@greg.holmes.name>
2017-08-18 23:04:05 +0100

    delete sites with integer

--- src/SiteRoutes.php
+++ src/SiteRoutes.php
@@ -74,7 +74,7 @@
                 }
             });
             $app->post('/site/delete/{siteid}', function(Application $app, $siteid) {
-                $siteid = filter_var($siteid, FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
+                $siteid = filter_var(intval($siteid), FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
                 try {
                     $app['stock']->deleteSite($siteid);
                     return $app->redirect('/site/manage');
